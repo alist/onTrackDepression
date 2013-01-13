44b44e5676b5f9c32caf73221a4e45c61f907a12
Alexander List <alist@listprojects.com>
2013-01-12 19:05:22 -0500

    added restkit the hardway with no damn cocoa beans

--- Domo/EXQIDSManager.m
+++ Domo/EXQIDSManager.m
@@ -7,6 +7,7 @@
 //
 
 #import "EXQIDSManager.h"
+#import <RestKit/RestKit.h>
 
 @interface EXQIDSManager ()
 
@@ -38,6 +39,7 @@
 }
 
 -(BOOL) qidsSubmissionsAvailableForAuthor:(EXAuthor*)author{
+	
 	return TRUE;
 }
 -(EXQIDSSubmission*) qidsSubmissionForAuthor:(EXAuthor*)author{
